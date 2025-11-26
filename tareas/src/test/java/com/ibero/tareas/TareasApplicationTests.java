77d1736e7e6ce1a947c5b817522ac48224f89807
Emmanuel Lopez Guerrero <manelpz@icloud.com>
2025-11-25 21:26:36 -0600

    Add files via upload

---
+++ tareas/src/test/java/com/ibero/tareas/TareasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibero.tareas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TareasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
